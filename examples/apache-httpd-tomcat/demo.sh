3b77286d0f3365ce501ad4bb2637064e5547f6bc
Mario Kleinsasser <mario.kleinsasser@gmail.com>
2018-02-10 10:14:03 +0100

    Change some configurations to fix examples
    Signed-off-by: Mario Kleinsasser <mario.kleinsasser@gmail.com>

--- examples/apache-httpd-tomcat/demo.sh
+++ examples/apache-httpd-tomcat/demo.sh
@@ -7,21 +7,34 @@ else
   echo "Swarm mode not enabled. Exiting!"; exit
 fi
 
+# First build the df-bosnd-httpd
+cd df-bosnd-httpd
+
 # Copy the bosnd binary to the build directory
-cp ../../bosnd .
+cp ../../../bosnd .
 
 # Build the Apache httpd container including the bosnd binary
 # For more informaiton see Dockerfile
-docker build . -t bosnd-httpd-tomcat:1.0
+docker build . -t bosnd-httpd:1.1
 
 # Remove the bosnd binary after build
 rm bosnd
 
+# Now build the corresponding df-bosnd-tomcat
+cd ../df-bosnd-tomcat
+
+# Build the Apache httpd container including the bosnd binary
+# For more informaiton see Dockerfile
+docker build . -t bosnd-tomcat:1.0
+
 # After the build create the overlay network
 docker network create --attachable --driver=overlay bosnd_net
 
+# go back home and start the stack
+cd ..
+
 # Start the stack
-docker stack deploy -c docker-stack.yml bosnd
+docker stack deploy -c docker-stack.yml pwd
 
 # Wait for user input
 sleep 2
@@ -29,4 +42,4 @@ echo -n "Docker stack started! Point your browser to http://127.0.0.1:3456! Pres
 read
 
 # Remove the stack
-docker stack rm bosnd
+docker stack rm pwd
